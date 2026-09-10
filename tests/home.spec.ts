c428eb0722de2f81dddf0e6e450a82b42782db6f
Luiz Eduardo <luiz.eduardop@outlook.com>
2026-09-09 22:59:20 -0300

    Ajustando teste de validação do título e adicionando um novo para validação do slogan da página inicial

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,6 +1,12 @@
 import { test, expect } from '@playwright/test'
 
-test('Deve exibir o slogan na aba do navegador', async ({ page }) => {
+test('Deve validar o título na aba do navegador', async ({ page }) => {
   await page.goto('http://localhost:3000/')
   await expect(page).toHaveTitle(/Lunar/)
+})
+
+test('Deve exibir o slogan na home page', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+  const slogan = page.getByRole('heading', {name:'Sua viagem para a Lua começa aqui.'})
+  await expect(slogan).toBeVisible()
 })
